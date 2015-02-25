362e2fa6f5af6fb9309da6da741491bfab0c0a65
Will Sloan <sloan.848@osu.edu>
2015-02-24 20:58:17 -0500

    Added torrent download functionality

--- js/movieListUtil.js
+++ js/movieListUtil.js
@@ -1,21 +1,17 @@
 function parseUpdate(url){
-    Papa.parse(url, { 
+    Papa.parse(url, {
         download: true,
         complete: function(results, file) {
             updateElement(results.data);
-            //buildDatabase(results.data);
         },
         error: function(error, file) {
             console.log(error, " in file ", file);
         }
-    })
-};
+    });
+}
 
 function updateElement(movieList){
-    
-    globalMovieList = movieList;
-    var fO = "";
-    
+
     var movieTable = document.createElement("TABLE");
     movieTable.className = "table table-striped table-hover";
 
@@ -26,50 +22,88 @@ function updateElement(movieList){
     cell1.innerHTML = "Movie";
     cell2.innerHTML = "Status";
 
+    /* Initialize click handler */
+    var createClickHandler = function(arg) {
+      return function() { movieLookup(arg); };
+    };
+
+    /* Generate each entry in the movie table */
     for(i = 1; i < movieList.length; i++){
-        if(movieList[i][1] != ""){
+        if(movieList[i][1] !== ""){
             var newRow = movieTable.insertRow(-1);
             var movieN = newRow.insertCell(0);
             var movieS = newRow.insertCell(1);
 
             tempMovieName = movieList[i][1];
-            movieN.innerHTML = movieList[i][1]; 
+            movieN.innerHTML = movieList[i][1];
             movieS.innerHTML = movieList[i][2];
 
-            var createClickHandler = function(arg) {
-              return function() { movieLookup(arg); };
-            };
             newRow.onclick = createClickHandler(movieList[i][1]);
         }
     }
-    $( "#fileOutput" ).replaceWith(movieTable);
-};
 
-function showPopup(movieData){
-  if(movieData.Plot == "N/A"){
-    console.log('Movie not Found.');
-    return;
-  }
-  var sweetText = movieData.Plot + '\n Rating: ' + movieData.imdbRating + '/10';
-  swal({
-    title: movieData.Title,
-    text: sweetText,
-    imageUrl: movieData.Poster,
-    allowOutsideClick: true
-  });
-
+    /* Fill fileOutuput field with the generated table */
+    $( "#movie_table_output" ).replaceWith(movieTable);
 }
 
 function movieLookup(title){
-  $.getJSON("http://www.omdbapi.com/",
-    {
-      t: title
-    },
-    function(data, status, xhr){
-      showPopup(data);
-    });
+  /* Callback goes
+   * lookupInfo -> lookupTorrent -> displayPopup
+   */
+  var lookupTorrent = function(movie_title, movie_info){
+    var lookupUrl = "https://yts.re/api/v2/list_movies.json?query_term=" + encodeURI(movie_title);
+    $.getJSON(
+        lookupUrl,
+        function(data, status, xhr){
+          console.log(movie_info);
+          console.log(data);
+          showPopup(movie_info, data);
+        }
+    );
+  };
+  var lookupInfo = function(movie_title){
+    $.getJSON("http://www.omdbapi.com/",
+      {
+        t: title
+      },
+      function(data, status, xhr){
+        if (data.Plot == "N/A") {
+          console.log("Movie not found.");
+          return 0;
+        }
+        lookupTorrent(title, data);
+      }
+    );
+  };
+  lookupInfo(title);
 }
 
-    
+function showPopup(movie_info, torrent_info){
+  var string_movie_info = movie_info.Plot + '\n Rating: ' + movie_info.imdbRating + '/10';
+  if (torrent_info.data.movie_count === 0){
+    swal({
+      title: movie_info.Title,
+      text: string_movie_info,
+      imageUrl: movie_info.Poster,
+      allowOutsideClick: true
+    });
+  } else {
+    var torrentUrl = torrent_info.data.movies[0].torrents[0].url;
+    swal({
+      title: movie_info.Title,
+      text: string_movie_info,
+      imageUrl: movie_info.Poster,
+      allowOutsideClick: true,
+      showCancelButton: true,
+      cancelButtonText: "OK",
+      //cancelButtonColor:
+      confirmButtonText: "Download"
+    }, function(){
+      window.location.replace(torrentUrl);
+      console.log(torrentUrl);
+    });
+  }
+}
+
 //main code
 parseUpdate("data/DVD_Log.csv");
